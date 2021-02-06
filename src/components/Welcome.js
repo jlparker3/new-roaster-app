397c090edcc63823ad96480cd2f0732c71beeb0f
Jessica Parker <jessicaleighparker3@gmail.com>
2021-02-06 14:07:25 -0600

    welcome conditional

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,16 @@
 function Welcome() {
+  const currentHour = new Date().getHours()
+  //if current hour is less than 5 then print why are you still up
+  //has to be more specific ones first then do a catch all which in this case is "good evening"
+  const message = currentHour < 5 ? "Why are you still up?" 
+                : currentHour < 12 ? "Good Morning" 
+                : currentHour < 18 ? "Good Afternoon"
+                : "Good Evening"
+
     return (
       <div>
+        {message} sports fans!
+
         <h1>Welcome Sports Fans!</h1>
       </div>
     );
